2217563a5d65c57ae9a1e790c6b73b79ea027e37
ruddyprima <primahendra70@gmail.com>
2020-04-22 20:59:18 +0700

    Tugas Perhitungan Array

---
+++ src/Tugas/Program_Do_While.java
@@ -0,0 +1,24 @@
+package Tugas;
+import java.util.Scanner;
+public class Program_Do_While {
+
+    public static void main(String[] args) {
+        Scanner masuk=new Scanner(System.in);
+        int m;
+        float n, jumlah, x, rata;
+        System.out.print("Banyaknya data : ");
+        n = masuk.nextFloat();
+        jumlah=0;
+        m=1;
+        do
+        {
+            System.out.print("Data ke-"+m+" : ");
+            x=masuk.nextFloat();
+            jumlah += x;
+            m++;
+        }while ( m<=n );
+        {
+            System.out.println("rata-rata :"+jumlah/n);
+        }
+    }
+}
